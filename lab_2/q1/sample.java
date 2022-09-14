0bcb117af6d650fdb843ea57bf8ee80d074d2882
iDream2 <akashdebn2@gmail.com>
2022-09-14 23:19:42 +0530

    initial commit

---
+++ lab_2/q1/sample.java
@@ -0,0 +1,19 @@
+class sample
+{
+public static void main(String args[ ])
+{
+int a = 10, b = 20;
+if(a > b){
+System.out.println(a + "is greater than" + b);
+}
+
+else if (b > a){
+System.out.println(b + "is greater than" + a);
+}
+else {
+System.out.println(a + "is equal to" + b);
+}
+
+}
+
+}
---
+++ lab_2/q2/sample.java
@@ -0,0 +1,13 @@
+class sample
+{
+int a = 10, b = 20;
+int  sum = a + b;
+System.out.println("The sum of a and b is " + sum);
+int difference = a - b;
+System.out.println("The sum of a and b is " + difference);
+int product = a*b;
+System.out.println("The sum of a and b is " + product);
+int quotient = a/b;
+System.out.println("The sum of a and b is " + quotient);
+
+}
